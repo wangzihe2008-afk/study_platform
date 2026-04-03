a6397abbbd5282150d46519917fca76ea4a657ca
wangzihe2008-afk <wangzihe2008@gmail.com>
2026-04-03 16:08:25 -0700

    fix port

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
